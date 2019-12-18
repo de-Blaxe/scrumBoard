96598ce1beabdd4a4c3beec3bc50cd86f21cc831
Donald Lieu <dclieu@ucdavis.edu>
2019-12-17 21:10:00 -0800

    Added deletion functionality

--- scrumboard/static/scrumboard/card.directive.js
+++ scrumboard/static/scrumboard/card.directive.js
@@ -15,7 +15,19 @@
                 };
                 $scope.modelOptions = {
                     debounce:500
-                }
+                };
+                $scope.delete = function() {
+                    $http.delete(url).then(
+                        function() {
+                            var cards = $scope.list.cards;
+                            cards.splice(
+                                cards.indexOf($scope.card),
+                                1
+                            );
+                        }
+                    );
+                };
+
             }]
 
         };
